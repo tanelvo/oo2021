818daf39eaf7634958b1267afdfd0ec03eea2918
Tanel Volkov <70900278+tanelvo@users.noreply.github.com>
2021-02-12 01:58:30 +0200

    Add files via upload

---
+++ w01_funktsioon/Homework.java
@@ -0,0 +1,9 @@
+public class Homework {
+    public static void main(String[] args) {
+        Float celsius = -15f;
+        
+        Float fahrenheit = ((celsius*9)/5)+32;
+        System.out.println("The temperature is "+ fahrenheit + " degrees fahrenheit!");  
+    }
+}
+ 
