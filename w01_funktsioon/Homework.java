5ffb745474f0f1789f90e8c4cd8ac5aba59421db
Tanel Volkov <70900278+tanelvo@users.noreply.github.com>
2021-02-12 13:24:32 +0200

    Delete Homework.java

--- w01_funktsioon/Homework.java
+++
@@ -1,9 +0,0 @@
-public class Homework {
-    public static void main(String[] args) {
-        Float celsius = -15f;
-        
-        Float fahrenheit = ((celsius*9)/5)+32;
-        System.out.println("The temperature is "+ fahrenheit + " degrees fahrenheit!");  
-    }
-}
- 
