266e803412ae2122863bfbf4ee1c0ccc1e1c8ee3
Tanel Volkov <70900278+tanelvo@users.noreply.github.com>
2021-05-13 00:08:48 +0300

    2/3

--- w13_kontrolltoo2/main.java
+++ w13_kontrolltoo2/main.java
@@ -1,12 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 // **Sõnade lõputähed**
@@ -17,14 +11,15 @@ import java.util.Scanner;
 
 
 public class main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         lastLetter();
     }
 
     public static void lastLetter(){
         ArrayList<String> text = new ArrayList<String>();
+        ArrayList<String> cleanText = new ArrayList<String>();
+        ArrayList<String> eachLetter = new ArrayList<String>();
         File textFromFile = new File("text.txt");
-
         // Exception scanneri pärast :(
         try{
         Scanner txt = new Scanner(textFromFile);
@@ -34,6 +29,83 @@ public class main {
         } catch (FileNotFoundException txt){
             System.out.println("Tekkis viga");
         }
+
+
+
+        int wordCount = text.size();
         
-    }
+        for(int i = 0; i < wordCount; i++){
+
+            
+
+            if((text.get(i)).contains(",")){
+                cleanText.add(((text.get(i)).replace(",","")).toLowerCase());
+            }
+            else if((text.get(i)).contains("-")||(text.get(i)).contains("-")){
+            }
+            else if((text.get(i)).contains(".")){
+                cleanText.add(((text.get(i)).replace(".","")).toLowerCase());
+            }
+            else if((text.get(i)).contains("?")){
+                cleanText.add(((text.get(i)).replace("?","")).toLowerCase());
+            }
+            else if((text.get(i)).contains("!")){
+                cleanText.add(((text.get(i)).replace("!","")).toLowerCase());
+            }
+            else if((text.get(i)).contains(";")){
+                cleanText.add(((text.get(i)).replace(";","")).toLowerCase());
+            }
+            else if((text.get(i)).contains(":")){
+                cleanText.add(((text.get(i)).replace(":","")).toLowerCase());
+            }
+            else if((text.get(i)).contains("'")){
+                cleanText.add(((text.get(i)).replace("'","")).toLowerCase());
+            } else {
+                cleanText.add((text.get(i)).toLowerCase());
+            }
+
+            
+        }   
+        //System.out.println(cleanText);
+        for (int i = 0; i < cleanText.size(); i++) {
+            int wordLength = (cleanText.get(i)).length();           
+            String letter = (cleanText.get(i)).substring(wordLength-1,wordLength);
+            eachLetter.add(letter);
+        }
+        //System.out.println(eachLetter);
+
+        //
+        StringBuffer sb = new StringBuffer();
+        for (String s : eachLetter) {
+           sb.append(s);
+           sb.append(" ");
+        }
+        String str = sb.toString();
+        System.out.println(str);
+
+        int frequency = 0 ;
+        char character = ' ' ;
+        String linesOfText = " " ; 
+        char[] alphabet = new char[26] ; 
+        for(char ch = 'a'; ch <= 'z'; ++ch)
+        {
+            alphabet[ch-'a']=ch ;
+        } 
+        linesOfText = str;
+        System.out.println("Täht          Sagedus") ;
+        for (int i = 0; i < alphabet.length; i++) 
+        {   frequency = 0 ;
+            for (int j = 0; j < linesOfText.length(); j++) {
+                character = linesOfText.charAt(j) ;
+                if (character == alphabet[i]) {
+                    frequency++ ;
+                }
+            }
+
+            System.out.println(alphabet[i] + "\t\t" + frequency) ;
+        }
+
+        }
+        
 }
+
